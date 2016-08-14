bd5ce2c2f61ce34f66264df873455cf88471aa17
Dexion <dexter0ion@gmail.com>
2016-08-14 17:22:14 +0800

    Add files via upload

---
+++ Assets/Scripts/CameraConroller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraConroller : MonoBehaviour {
+
+	public GameObject Player;
+
+	private Vector3 diff;
+
+	// Use this for initialization
+	void Start () {
+		diff = transform.position - Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = Player.transform.position+diff;
+	}
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+
+	public float strength;
+    
+	private Rigidbody rb;
+
+	private int count;
+
+	public Text CountText;
+	public Text WinText;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		TextUpdate ();
+		WinText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * strength);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Stones"))
+			{
+			other.gameObject.SetActive(false);
+			count += 1;
+			TextUpdate ();
+			}
+	}
+
+	void TextUpdate()
+	{
+		CountText.text = "Count: " + count.ToString ();
+
+		if (count >= 18) 
+		{
+			WinText.text = "You Win";
+		}
+	}
+}
